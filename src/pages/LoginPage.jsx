a186265d2f0408d73612502a07355628ea3a9ba5
Kevin Valani <valanikevin@gmail.com>
2023-07-19 19:28:24 -0600

    added logo, changed theme color

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useContext } from "react";
 import AuthContext, { useAuth } from "../utils/AuthContext";
 import { useNavigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import { Col, Row, Card, Form, Button, Image } from "react-bootstrap";
+// import media files
+import Logo from "../assets/images/brand/logo/logo.svg";
 const LoginPage = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
@@ -15,20 +18,74 @@ const LoginPage = () => {
   let { loginUser } = useContext(AuthContext);
 
   return (
-    <div>
-      <form onSubmit={loginUser}>
-        <br /> <br />
-        <input type="email" name="email" placeholder="Enter your email" />
-        <br /> <br />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />{" "}
-        <br /> <br />
-        <input type="submit" />
-      </form>
-    </div>
+    <>
+      <Row className="align-items-center justify-content-center g-0 min-vh-100">
+        <Col lg={5} md={5} className="py-8 py-xl-0">
+          <Card>
+            <Card.Body className="p-6">
+              <div className="mb-4">
+                <Link to="/">
+                  <Image src={Logo} className="mb-4" alt="" />
+                </Link>
+                <h1 className="mb-1 fw-bold">Sign in</h1>
+                <span>
+                  Don’t have an account?{" "}
+                  <Link to="/authentication/sign-up" className="ms-1">
+                    Sign up
+                  </Link>
+                </span>
+              </div>
+              {/* Form */}
+              <Form onSubmit={loginUser}>
+                <Row>
+                  <Col lg={12} md={12} className="mb-3">
+                    {/* Username or email */}
+                    <Form.Label>Username or email </Form.Label>
+                    <Form.Control
+                      type="email"
+                      id="email"
+                      placeholder="Email address here"
+                      required
+                    />
+                  </Col>
+                  <Col lg={12} md={12} className="mb-3">
+                    {/* Password */}
+                    <Form.Label>Password </Form.Label>
+                    <Form.Control
+                      type="password"
+                      id="password"
+                      placeholder="**************"
+                      required
+                    />
+                  </Col>
+                  <Col lg={12} md={12} className="mb-3">
+                    {/* Checkbox */}
+                    <div className="d-md-flex justify-content-between align-items-center">
+                      <Form.Group
+                        className="mb-3 mb-md-0"
+                        controlId="formBasicCheckbox"
+                      >
+                        <Form.Check type="checkbox" label="Remember me" />
+                      </Form.Group>
+                      <Link to="/authentication/forget-password">
+                        Forgot your password?
+                      </Link>
+                    </div>
+                  </Col>
+                  <Col lg={12} md={12} className="mb-0 mt-4 d-grid gap-2">
+                    {/* Button */}
+                    <Button variant="primary" type="submit">
+                      Sign in
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+              <div className="mb-4" />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
