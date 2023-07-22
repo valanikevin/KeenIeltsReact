af92e4d907f4f224204d28f069f1bde14911bf59
Kevin Valani <valanikevin@gmail.com>
2023-07-22 12:43:45 -0600

    added formik and yup validation

--- src/components/layout/BaseForm.jsx
+++ src/components/layout/BaseForm.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useContext, useState } from "react";
 import AuthContext, { useAuth } from "../../utils/AuthContext";
 import { Link } from "react-router-dom";
+import { Formik, Field, Form as FormikForm } from "formik";
+import * as Yup from "yup";
+
 import {
   Col,
   Row,
@@ -12,51 +15,76 @@ import {
 } from "react-bootstrap";
 import NotificationContext from "../../context/layout/NotificationContext";
 
-const BaseForm = ({ form_fields, submit_label, on_submit, errors }) => {
-  const [validated, setValidated] = useState(false);
+const BaseForm = ({
+  form_fields,
+  submit_label,
+  on_submit,
+  errors,
+  validation_schema,
+}) => {
+  let initialValues = {};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    if (form.checkValidity() === false) {
-      e.stopPropagation();
-    } else {
-      on_submit(e);
-    }
+  form_fields.forEach(function (field) {
+    initialValues[field.id] = "";
+  });
 
-    setValidated(true);
-  };
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row>
-        {form_fields.map((field) => (
-          <FormGroup as={Col} lg={12} md={12} className="mb-3" key={field.id}>
-            <Form.Label>{field.label}</Form.Label>
-            <Form.Control
-              type={field.type}
-              id={field.id}
-              placeholder={field.placeholder}
-              isInvalid={true}
-              required
-            />
-            {errors &&
-              errors[field.id] &&
-              errors[field.id].map((error, index) => (
-                <Form.Control.Feedback type="invalid" key={index}>
-                  {error.message}
-                </Form.Control.Feedback>
-              ))}
-          </FormGroup>
-        ))}
+    <>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+        validationSchema={validation_schema}
+      >
+        {({ errors, touched, isValid, dirty }) => (
+          <FormikForm>
+            {form_fields.map((field) => (
+              <FormGroup
+                as={Col}
+                lg={12}
+                md={12}
+                className="mb-3"
+                key={field.id}
+              >
+                <Form.Label htmlFor={field.id}>{field.label}</Form.Label>
+                <Field
+                  as={Form.Control}
+                  id={field.id}
+                  name={field.id}
+                  placeholder={field.placeholder}
+                  isInvalid={
+                    errors[field.id] && touched[field.id] ? true : false
+                  }
+                  isValid={
+                    errors[field.id] === undefined && touched[field.id]
+                      ? true
+                      : false
+                  }
+                />
+                {errors[field.id] && touched[field.id] ? (
+                  <Form.Control.Feedback type="invalid">
+                    {errors[field.id]}
+                  </Form.Control.Feedback>
+                ) : null}
+              </FormGroup>
+            ))}
 
-        <Col lg={12} md={12} className="mb-0 mt-4 d-grid gap-2">
-          {/* Button */}
-          <Button variant="primary" type="submit">
-            {submit_label}
-          </Button>
-        </Col>
-      </Row>
-    </Form>
+            <Col lg={12} md={12} className="mb-0 mt-4 d-grid gap-2">
+              {/* Button */}
+              <Button
+                variant="primary"
+                type="submit"
+                disabled={!(isValid && dirty)}
+              >
+                {submit_label}
+              </Button>
+            </Col>
+          </FormikForm>
+        )}
+      </Formik>
+    </>
   );
 };
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,17 +1,13 @@
 import { useEffect, useContext, useState } from "react";
 import AuthContext, { useAuth } from "../utils/AuthContext";
 import { Link } from "react-router-dom";
-import {
-  Col,
-  Row,
-  Card,
-  Form,
-  Button,
-  Alert,
-  FormGroup,
-} from "react-bootstrap";
+import { Col, Row, Card, Button, Alert, FormGroup } from "react-bootstrap";
 import NotificationContext from "../context/layout/NotificationContext";
 import BaseForm from "../components/layout/BaseForm";
+import * as Yup from "yup";
+import YupPassword from "yup-password";
+
+YupPassword(Yup);
 
 const RegisterPage = () => {
   let { registerUser, registrationError } = useContext(AuthContext);
@@ -54,6 +50,32 @@ const RegisterPage = () => {
     },
   ];
 
+  const SignupSchema = Yup.object().shape({
+    first_name: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    last_name: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    password1: Yup.string()
+      .required("Password is required")
+      .min(
+        5,
+        "password must contain 5 or more characters with at least one of each: uppercase, lowercase, number and special"
+      )
+      .minLowercase(1, "password must contain at least 1 lower case letter")
+      .minUppercase(1, "password must contain at least 1 upper case letter")
+      .minNumbers(1, "password must contain at least 1 number")
+      .minSymbols(1, "password must contain at least 1 special character"),
+    password2: Yup.string().oneOf(
+      [Yup.ref("password1"), null],
+      "Passwords must match"
+    ),
+  });
+
   return (
     <>
       <Row className="align-items-center justify-content-center g-0 ">
@@ -70,13 +92,14 @@ const RegisterPage = () => {
                 </span>
               </div>
 
-              {/* Form */}
               <BaseForm
                 form_fields={form_fields}
                 submit_label={"Sign up"}
                 on_submit={registerUser}
                 errors={registrationError}
+                validation_schema={SignupSchema}
               />
+
               <div className="mb-4" />
             </Card.Body>
           </Card>
