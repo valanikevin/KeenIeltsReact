0bc76fccb9be91662d6b870d1243c0ba185eb7df
Kevin Valani <valanikevin@gmail.com>
2023-07-31 19:20:08 -0600

    test

--- src/pages/ieltstest/AttemptListeningModulePage.jsx
+++ src/pages/ieltstest/AttemptListeningModulePage.jsx
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
-import SplitPane, { Pane } from "split-pane-react";
-import "split-pane-react/esm/themes/default.css";
+import SplitPane, { Pane } from "react-split-pane";
 
 const AttemptListeningModulePage = () => {
-  const [sizes, setSizes] = useState([100, "30%", "auto"]);
-
-  const layoutCSS = {
-    height: "100%",
+  const paneStyle = {
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "column",
+    height: "100%",
+    overflow: "auto",
   };
   return (
-    <div style={{ height: 1000 }}>
-      <SplitPane split="horizontal" sizes={sizes} onChange={setSizes}>
-        <Pane minSize={50} maxSize="50%">
-          <div style={{ ...layoutCSS, background: "#ddd" }}>pane1</div>
-        </Pane>
-        <div style={{ ...layoutCSS, background: "#d5d7d9" }}>pane2</div>
-        <div style={{ ...layoutCSS, background: "#a1a5a9" }}>pane2</div>
-      </SplitPane>
-    </div>
+    <SplitPane split="horizontal" defaultSize={200} primary="second">
+      <div style={paneStyle}>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+        <h1>Kevin</h1>
+      </div>
+      <div />
+    </SplitPane>
   );
 };
 
