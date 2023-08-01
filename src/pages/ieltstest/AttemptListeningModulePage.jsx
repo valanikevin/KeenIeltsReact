d900cbc2c0ba0458149c80f74655737da6a37190
Kevin Valani <valanikevin@gmail.com>
2023-08-01 13:04:31 -0600

    added questions page for listening page

--- src/pages/ieltstest/AttemptListeningModulePage.jsx
+++ src/pages/ieltstest/AttemptListeningModulePage.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import useAxios from "../../utils/useAxios";
-import ReactAudioPlayer from "../../components/elements/audioplayer/ReactAudioPlayer";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "../../components/elements/audioplayer/ReactAudioPlayer.css";
+import { Row, Col, Container, Card } from "react-bootstrap";
+import parse from "html-react-parser";
 
 const AttemptListeningModulePage = () => {
   const { module_slug } = useParams();
   const api = useAxios();
-  const [module, setModule] = useState({});
+  const [module, setModule] = useState(null);
 
   useEffect(() => {
     getModule();
@@ -21,10 +25,52 @@ const AttemptListeningModulePage = () => {
     }
   }
 
+  if (!module) {
+    return null;
+  }
+
   return (
-    <div className="p-5">
-      <ReactAudioPlayer url="https://ielts-up.com/listening/1.2.mp3" />
-    </div>
+    <>
+      <Row className="sticky-top">
+        <Col>
+          <AudioPlayer
+            className=" border-top"
+            autoPlay={false}
+            src={module.sections[0].audio}
+            onPlay={(e) => console.log(module.sections[0].audio)}
+          />
+        </Col>
+      </Row>
+      <Container className="my-3">
+        <Row>
+          {module.sections.length > 0 &&
+            module.sections.map((section) => (
+              <Col key={section.id}>
+                <Card>
+                  <Card.Header>
+                    <span className="text-uppercase fw-bold text-black">
+                      {section.section}
+                    </span>
+                  </Card.Header>
+                  <Card.Body>
+                    <form className="text-black">
+                      {parse(section.questions, {
+                        replace: (domNode) => {
+                          if (domNode.name === "input") {
+                            return (
+                              <input className="m-2" {...domNode.attribs} />
+                            );
+                          }
+                        },
+                      })}
+                    </form>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    </>
   );
 };
 
