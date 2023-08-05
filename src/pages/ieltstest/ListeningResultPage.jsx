ff6f1ef954f74291296573130d4f91bd9be1d3c1
Kevin Valani <valanikevin@gmail.com>
2023-08-05 15:59:15 -0600

    added review answers

--- src/pages/ieltstest/ListeningResultPage.jsx
+++ src/pages/ieltstest/ListeningResultPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import useAxios from "../../utils/useAxios";
 import { API_URLS } from "../../utils/urls";
 import { useParams } from "react-router";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Accordion, Card, Col, Container, Row, Table } from "react-bootstrap";
 import PageHeadingBriefinfo from "../../components/layout/PageHeadingBriefInfo";
 
 const ListeningResultPage = () => {
@@ -39,6 +39,9 @@ const ListeningResultPage = () => {
         />
         <Container className="my-3">
           <Row className="px-2">
+            <Col xs={12}>
+              <h2 className="display4">Your Scores</h2>
+            </Col>
             <Col xs={6} md={3} className="my-2 d-flex">
               <Card className="flex-fill">
                 <Card.Body className="">
@@ -90,7 +93,59 @@ const ListeningResultPage = () => {
         </Container>
       </div>
 
-      <div></div>
+      <div className="my-4">
+        <Container>
+          <Row>
+            <Col sm={12} md={6}>
+                
+            </Col>
+            <Col sm={12} md={6}>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>
+                    <span className="text-black fw-bold">Review Answers</span>
+                  </Accordion.Header>
+                  <Accordion.Body className="p-0 m-0">
+                    <Table bordered className="table-sm">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Your Answer</th>
+                          <th scope="col">Correct Answer</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {Object.entries(attempt.evaluation.all_questions).map(
+                          (item, index) => (
+                            <tr
+                              key={index}
+                              className={
+                                item[1]["is_user_answer_correct"] === true
+                                  ? "table-success"
+                                  : "table-danger"
+                              }
+                            >
+                              <td className="fw-bold">{index + 1}</td>
+                              <td className="text-black">
+                                {item[1]["user_answer"]}
+                              </td>
+                              <td className="text-black">
+                                {item[1]["correct_answer"].length > 1
+                                  ? JSON.stringify(item[1]["correct_answer"])
+                                  : item[1]["correct_answer"]}
+                              </td>
+                            </tr>
+                          )
+                        )}
+                      </tbody>
+                    </Table>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
