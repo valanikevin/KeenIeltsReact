b0d48595372714daf30b86a04c478f14e7070bc6
Kevin Valani <valanikevin@gmail.com>
2023-08-10 12:10:57 -0600

    sample resize panel working

--- src/pages/ieltstest/AttemptReadingModulePage.jsx
+++ src/pages/ieltstest/AttemptReadingModulePage.jsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SplitPane from "react-split-pane";
+import "./ReactSplitPane.css";
+import { Card, Container } from "react-bootstrap";
 
 const AttemptReadingModulePage = () => {
-  return <div>AttemptReadingModulePage</div>;
+  const [deviceType, setDeviceType] = useState("desktop");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setDeviceType("mobile");
+      } else {
+        setDeviceType("desktop");
+      }
+    };
+
+    // Call once to set initial state
+    handleResize();
+
+    // Attach event listener for future window resize events
+    window.addEventListener("resize", handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array ensures this effect runs once on mount and once on unmount
+
+  return (
+    <div style={{ height: "90vh", width: "98vw" }}>
+      <SplitPane
+        split={deviceType === "mobile" ? "horizontal" : "vertical"}
+        minSize={100}
+        defaultSize={100}
+      >
+        <div className="simulationDiv p-2" style={{ overflow: "auto" }}>
+          <Card>
+            <Card.Body>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="statisticsDiv p-2" style={{ overflow: "auto" }}>
+          <Card>
+            <Card.Body>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+              <h1>Kevin </h1>
+            </Card.Body>
+          </Card>
+        </div>
+      </SplitPane>
+    </div>
+  );
 };
 
 export default AttemptReadingModulePage;
