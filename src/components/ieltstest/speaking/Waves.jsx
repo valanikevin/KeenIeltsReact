8bf7d1d5ed2e94e1c5a2f03b0d0df451c73ba2f2
Kevin Valani <valanikevin@gmail.com>
2023-08-31 23:20:13 -0600

    added waves

---
+++ src/components/ieltstest/speaking/Waves.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Waves.css";
+
+const Waves = ({ isSpeaking, background = "primary" }) => {
+  return (
+    <div
+      className={`d-flex justify-content-center align-items-center bg-${background}`}
+      style={{ height: "30px" }}
+    >
+      {isSpeaking === false ? (
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="waveform-bar animated rounded"></div>
+          <div className="waveform-bar animated rounded"></div>
+          <div className="waveform-bar animated rounded"></div>
+        </div>
+      ) : (
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="waveform-dot"></div>
+          <div className="waveform-dot"></div>
+          <div className="waveform-dot"></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Waves;
