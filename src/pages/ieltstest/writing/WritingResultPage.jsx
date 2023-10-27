ceba6652308e530ac41990bb935850d4678ae04d
Kevin Valani <valanikevin@gmail.com>
2023-10-27 14:42:35 -0600

    added writing frontend

--- src/pages/ieltstest/writing/WritingResultPage.jsx
+++ src/pages/ieltstest/writing/WritingResultPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import useAxiosWithoutLoader from "../../../utils/useAxiosWithoutLoader";
 import { API_URLS } from "../../../utils/urls";
 import PageHeadingBriefinfo from "../../../components/layout/PageHeadingBriefInfo";
@@ -13,17 +13,21 @@ import {
   ListGroup,
   Row,
   Stack,
+  Table,
 } from "react-bootstrap";
 import parse from "html-react-parser";
 import "../../../components/ieltstest/writing/WritingModule.css";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 import SectionCard from "../../../components/ieltstest/SectionCard";
+import useAxios from "../../../utils/useAxios";
 
 const WritingResultPage = () => {
   const { module_slug, attempt_slug } = useParams();
   const [module, setModule] = useState(null);
   const [attempt, setAttempt] = useState(null);
   const [currentSection, setCurrentSection] = useState(null);
+  const [evaluation, setEvaluation] = useState(null);
+
   const isFirstSection = currentSection
     ? currentSection.id === module.sections[0].id
     : false;
@@ -32,7 +36,8 @@ const WritingResultPage = () => {
     : false;
   const [deviceType, setDeviceType] = useState("desktop");
 
-  const api = useAxiosWithoutLoader();
+  const api1 = useAxiosWithoutLoader();
+  const api = useAxios();
 
   async function getAttempt() {
     const response = await api.post(
@@ -44,7 +49,6 @@ const WritingResultPage = () => {
       console.error("Unable to fetch attempt at ListeningResult.jsx");
     }
   }
-
   async function getModule() {
     const response = await api.post(
       API_URLS.getWritingModule + module_slug + "/"
@@ -52,6 +56,32 @@ const WritingResultPage = () => {
     if (response.status === 200) {
       setModule(response.data);
       setCurrentSection(response.data.sections[0]);
+    }
+  }
+
+  async function evaluateSections(sections, index) {
+    if (index < sections.length) {
+      await getEvaluation(sections[index].id);
+      evaluateSections(sections, index + 1); // Move to the next section
+    }
+  }
+
+  async function getEvaluation(section_id) {
+    const response = await api1.post(
+      "/ieltstest/get_writing_evaluation/" +
+        attempt_slug +
+        "/" +
+        section_id +
+        "/"
+    );
+
+    if (response.status === 200) {
+      setEvaluation((prevEvaluation) => ({
+        ...prevEvaluation,
+        [section_id]: response.data,
+      }));
+    } else {
+      console.error("Unable to fetch attempt at WritingResultPage.jsx");
     }
   }
 
@@ -92,6 +122,12 @@ const WritingResultPage = () => {
   }, []);
 
   useEffect(() => {
+    if (module && module.sections) {
+      evaluateSections(module.sections, 0); // Start with the first section
+    }
+  }, [module]);
+
+  useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 768) {
         setDeviceType("mobile");
@@ -125,7 +161,7 @@ const WritingResultPage = () => {
       />
 
       <Container className="mb-3">
-        <Row>
+        <Row className="justify-content-center">
           <Col sm={12} className="mt-3">
             <SectionCard
               currentSection={currentSection}
@@ -145,7 +181,157 @@ const WritingResultPage = () => {
               </Card.Body>
             </Card>
           </Col>
-          <Col sm={12} md={6}></Col>
+          <Col sm={12} md={6} className="mt-3">
+            <Card className="h-100">
+              <Card.Header>
+                <h3 className="mt-2 fw-bold">Estimated Band Scores</h3>
+              </Card.Header>
+              <Card.Body>
+                <Table striped bordered hover responsive size="sm">
+                  <thead>
+                    <tr>
+                      <th>Description</th>
+                      <th>Bands</th>
+                    </tr>
+                  </thead>
+                  <tbody className="">
+                    <tr>
+                      <th>
+                        <h3 className="m-0">TA: </h3>
+                        Task Achievement
+                      </th>
+                      <th>
+                        <h3>6.5</h3>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <h3 className="my-0">CC: </h3>
+                        Coherence and Cohesion
+                      </th>
+                      <th>
+                        <h3>6.5</h3>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <h3 className="m-0">LR: </h3>
+                        Lexical Resource
+                      </th>
+                      <th>
+                        <h3>6.5</h3>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <h3 className="m-0">GRA: </h3>
+                        Grammatical Range Accuracy
+                      </th>
+                      <th>
+                        <h3>6.5</h3>
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <h3 className="fw-bold mt-2">Overall </h3>
+                      </th>
+                      <th>
+                        <h3 className="fw-bold mt-2">6.5</h3>
+                      </th>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={12} md={6} className="mt-3">
+            <Card className="h-100">
+              <Card.Header>
+                <h3 className="mt-2 fw-bold">Bands Description</h3>
+              </Card.Header>
+              <Card.Body>
+                <Table striped bordered hover responsive size="sm">
+                  <thead>
+                    <tr>
+                      <th>Requirements</th>
+                      <th>Explanation</th>
+                    </tr>
+                  </thead>
+                  <tbody className="">
+                    <tr>
+                      <th>
+                        <h3 className="m-0">TA: </h3>
+                        Task Achievement
+                      </th>
+                      <th>
+                        Candidate addresses the requirements of the task and
+                        presents an overview with information appropriately
+                        selected. There may be inconsistencies in tone.
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <h3 className="my-0">CC: </h3>
+                        Coherence and Cohesion
+                      </th>
+                      <th>
+                        Candidate addresses the requirements of the task and
+                        presents an overview with information appropriately
+                        selected. There may be inconsistencies in tone.
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <h3 className="m-0">LR: </h3>
+                        Lexical Resource
+                      </th>
+                      <th>
+                        Candidate addresses the requirements of the task and
+                        presents an overview with information appropriately
+                        selected. There may be inconsistencies in tone.
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>
+                        <h3 className="m-0">GRA: </h3>
+                        Grammatical Range Accuracy
+                      </th>
+                      <th>
+                        Candidate addresses the requirements of the task and
+                        presents an overview with information appropriately
+                        selected. There may be inconsistencies in tone.
+                      </th>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={12} md={8} className="mt-3">
+            <Card>
+              <Card.Header>
+                <h3 className="mt-2 fw-bold">
+                  Vocabulary & Word Choice Suggestions
+                </h3>
+              </Card.Header>
+              <Card.Body>
+                <Table bordered hover responsive>
+                  <tbody className="">
+                    {evaluation &&
+                      evaluation[currentSection.id][
+                        "vocabulary_choice_suggestions"
+                      ].map((suggestion) => (
+                        <tr>
+                          <td>
+                            <p style={{ fontSize: "1.1rem" }}>{suggestion}</p>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
       </Container>
     </>
