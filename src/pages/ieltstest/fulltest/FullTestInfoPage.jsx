d897c8b3206732f702bfd660c28c0718e8db84c5
Kevin Valani <valanikevin@gmail.com>
2023-11-06 00:02:40 -0700

    added api to fetch full test info

--- src/pages/ieltstest/fulltest/FullTestInfoPage.jsx
+++ src/pages/ieltstest/fulltest/FullTestInfoPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useAxios from "../../../utils/useAxios";
+import { DJANGO_BASE_URL } from "../../../utils/config";
 
 const FullTestInfoPage = () => {
-  return (
-    <div>FullTestInfoPage</div>
-  )
-}
+  const { attempt_slug } = useParams();
+  const api = useAxios();
+  const [fullTest, setFullTest] = useState(null);
 
-export default FullTestInfoPage
+  async function getFullTestInfo() {
+    const response = await api.post(
+      DJANGO_BASE_URL + "/ieltstest/get_fulltest_info/" + attempt_slug + "/"
+    );
+    if (response.status === 200) {
+      setFullTest(response.data);
+    } else {
+      console.log("Error");
+    }
+  }
+
+  useEffect(() => {
+    getFullTestInfo();
+  }, []);
+
+  return <h1>FullTestInfoPage</h1>;
+};
+
+export default FullTestInfoPage;
