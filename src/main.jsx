259fc7b6ed6a4cdd331eff79c7086fce6211eff1
Kevin Valani <valanikevin@gmail.com>
2023-12-09 15:54:43 -0700

    Comment out ReactGA initialization and send functions

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import "./assets/scss/theme.scss";
 import { BrowserRouter } from "react-router-dom";
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-0Q7F9F29C5");
+// ReactGA.initialize("G-0Q7F9F29C5");
 
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
+// ReactGA.send({
+//   hitType: "pageview",
+//   page: window.location.pathname,
+// });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
